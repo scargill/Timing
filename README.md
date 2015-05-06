0bebb344b9d72da1a151c4f1dcfec76d79a781df
Peter Scargill <pete@scargill.org>
2015-05-06 02:14:07 +0100

    Removed some rubbish

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-# Timing
-<<<<<<< HEAD
 A simple timer class for Arduino - no interrupts, no overflow problems. 2 options,
 one of which catches up if late, the other does not.
 
- For more on IOT, Arduino, ESP8266 and Raspberry Pi visit http://tech.scargill.net
+For more on IOT, Arduino, ESP8266 and Raspberry Pi visit http://tech.scargill.net
 
-
-=======
-A simple timer class for Arduino - no interrupts, no overflow problem
-
- For more on IOT, Arduino, ESP8266 and Raspberry Pi visit http://tech.scargill.net
--- Timing.h
+++ Timing.h
@@ -1,19 +1,13 @@
-//
-<<<<<<< HEAD
-// easy to use timing code that handles millis() overflow
-=======
+
 // easy to use timing that handles millis() overflow
->>>>>>> b45aa3a599be3dc324313397f49c4c0f9c6956d0
 // example:
 // create timer:    Timing mytimer;
 // in setup():      mytimer.begin(0); // 0=catchup, 1=always use delay even if late
 // in loop():       if (mytimer.isOver(1000) {
 //                                           do something every 1000ms
 //                                           }
-<<<<<<< HEAD
 // This file Copyright (c) 2015 Peter Scargill
 =======
->>>>>>> b45aa3a599be3dc324313397f49c4c0f9c6956d0
 //
 #ifndef Timing_h
 #define Timing_h
@@ -24,7 +18,7 @@ class Timing
 private:
  unsigned long counter;
  boolean catchup;
-<<<<<<< HEAD
+
 public:
     void begin(boolean cat)
     {
@@ -59,37 +53,36 @@ public:
 };
 #endif
 =======
-public: 
-	void begin(boolean cat)
-	{
-		counter=millis();
-		catchup=cat;
-	}
-		
-	boolean onTimeout(unsigned long interval)
-	{
-		unsigned long newmillis=millis();  // temp vars so easier comparison
-		if ((newmillis-counter)>=interval)
-			{
-			if (catchup) counter=newmillis; else counter+=interval;
-			return true;
-			}
-		return false;
-	}
-	
-	
-	void onTimeout(unsigned long interval, void (*g)()) // this version pass function as parameter
-	{
-		unsigned long newmillis=millis();  // temp vars so easier comparison
-		if ((newmillis-counter)>=interval)
-			{
-			if (catchup) counter=newmillis; else counter+=interval;
-			g();
-			}
-	}
-	
-	
+public:
+    void begin(boolean cat)
+    {
+        counter=millis();
+        catchup=cat;
+    }
+
+    boolean onTimeout(unsigned long interval)
+    {
+        unsigned long newmillis=millis();  // temp vars so easier comparison
+        if ((newmillis-counter)>=interval)
+            {
+            if (catchup) counter=newmillis; else counter+=interval;
+            return true;
+            }
+        return false;
+    }
+
+
+    void onTimeout(unsigned long interval, void (*g)()) // this version pass function as parameter
+    {
+        unsigned long newmillis=millis();  // temp vars so easier comparison
+        if ((newmillis-counter)>=interval)
+            {
+            if (catchup) counter=newmillis; else counter+=interval;
+            g();
+            }
+    }
+
+
 
 };
 #endif
->>>>>>> b45aa3a599be3dc324313397f49c4c0f9c6956d0
